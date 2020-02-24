dc6c2d40b756c90417d99812261fd3d404d88cad
anastasnikiforova <61436062+anastasnikiforova@users.noreply.github.com>
2020-02-25 00:39:46 +0300

    Add files via upload

---
+++ new_js.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+              }
+            }
+        ]
+    });
+    console.log( "ready!" );
+    $(".multiple-items").on('afterChange', function(event, slick, currentSlide){
+      $("#message").text(currentSlide + 1);
+   });
+  }); 
